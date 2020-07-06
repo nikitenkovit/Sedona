aecb7805d56b23797f2638deb8921114bb5074ef
Vitaliy <nelot@mail.ru>
2020-07-07 00:11:55 +0300

    add pop-up style and js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 (function () {
-  const closeButton = document.querySelector(".main-nav__button-close");
-  const openButton = document.querySelector(".main-nav__button-open")
-  const navLists = document.querySelectorAll(".main-nav__list")
+  "use strict";
+  //show and hidden main navigation
+  let closeButton = document.querySelector(".main-nav__button-close");
+  let openButton = document.querySelector(".main-nav__button-open");
+  let navLists = document.querySelectorAll(".main-nav__list");
 
-  openButton.onclick = function () {
+  openButton.addEventListener('click', function () {
     for (let navList of navLists) {
       navList.classList.add("show");
     }
-  }
+  });
 
-  closeButton.onclick = function () {
+  closeButton.addEventListener('click', function () {
     for (let navList of navLists) {
       if (navList.classList.contains("show")) {
         navList.classList.remove("show");
       }
     }
+  });
+
+  //Pop-up
+  let popUps = document.querySelectorAll(".pop-up__wrapper");
+  let popUpButtons = document.querySelectorAll(".pop-up__button");
+  let formButton = document.querySelector(".feedback-form__button");
+  let popUpSuccess = document.getElementById("success");
+
+  formButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popUpSuccess.classList.add("pop-up--show");
+  })
+
+  for (let i = 0; i < popUps.length; i++) {
+    popUpButtons[i].addEventListener('click', function () {
+      popUps[i].classList.remove("pop-up--show")
+    });
+    document.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        popUps[i].classList.remove("pop-up--show");
+      }
+    });
   }
 })()
--- js/thumbsUp.js
+++ js/thumbsUp.js
@@ -1,16 +1,15 @@
 (function () {
-  const thumbsUpButtons = document.querySelectorAll(".thumbs-up-button");
+  let thumbsUpButtons = document.querySelectorAll(".thumbs-up-button");
   let likesNumbers = document.querySelectorAll(".thumbs-up-button__likes-number");
 
   for (let i = 0; i < thumbsUpButtons.length; i++) {
     thumbsUpButtons[i].onclick = function () {
-      if (thumbsUpButtons[i].classList.contains("thumbs-up-button--positive")) {
-        thumbsUpButtons[i].classList.remove("thumbs-up-button--positive");
+      if (thumbsUpButtons[i].classList.contains("thumbs-up-button--added")) {
         likesNumbers[i].textContent--;
       } else {
-        thumbsUpButtons[i].classList.add("thumbs-up-button--positive");
         likesNumbers[i].textContent++;
       }
+      thumbsUpButtons[i].classList.toggle("thumbs-up-button--added");
     }
   }
 })()
